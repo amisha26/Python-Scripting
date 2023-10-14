2b5f8fc6ace86b86b2f3515d31bff2aff4d62cbc
amisha26 <amisha8299@gmail.com>
2023-10-14 15:21:08 -0400

    copy data from source to destination done

--- get_game_data.py
+++ get_game_data.py
@@ -1,48 +1,12 @@
+# ======= Lib imports =========
 import os
 import json
-import shutil
+import shutil 
 from subprocess import PIPE, run
-import sys
+import sys 
 
 
 GAME_DIR_PATTERN = "game"
-GAME_CODE_EXTENSION = ".go"
-GAME_COMPILE_COMMAND = ["go", "build"]
-
-
-def find_all_game_paths(source):
-    game_paths = []
-
-    for root, dirs, files in os.walk(source):
-        for directory in dirs:
-            if GAME_DIR_PATTERN in directory.lower():
-                path = os.path.join(source, directory)
-                game_paths.append(path)
-
-        break
-
-    return game_paths
-
-
-def get_name_from_paths(paths, to_strip):
-    new_names = []
-    for path in paths:
-        _, dir_name = os.path.split(path)
-        new_dir_name = dir_name.replace(to_strip, "")
-        new_names.append(new_dir_name)
-
-    return new_names
-
-
-def create_dir(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-
-def copy_and_overwrite(source, dest):
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    shutil.copytree(source, dest)
 
 
 def make_json_metadata_file(path, game_dirs):
@@ -50,36 +14,49 @@ def make_json_metadata_file(path, game_dirs):
         "gameNames": game_dirs,
         "numberOfGames": len(game_dirs)
     }
-
     with open(path, "w") as f:
         json.dump(data, f)
 
 
-def compile_game_code(path):
-    code_file_name = None
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(GAME_CODE_EXTENSION):
-                code_file_name = file
-                break
 
+# Fetches name from the path
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+    return new_names
+
+
+# Creates directory at a given path
+def create_directory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+# Copy source to destination and if directory exists overwrite it
+def copy_and_overwrite(source, destination):
+    if os.path.exists(destination):
+        # recursive copy
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
+
+
+# Find all the game path in the data directory
+def find_all_game_paths(source):
+    game_paths = []
+    for root, dirs, files in os.walk(source):
+
+        # Give names of all directories and not their paths
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
         break
+    return game_paths
 
-    if code_file_name is None:
-        return
-
-    command = GAME_COMPILE_COMMAND + [code_file_name]
-    run_command(command, path)
-
-
-def run_command(command, path):
-    cwd = os.getcwd()
-    os.chdir(path)
-
-    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
-    print("compile result", result)
-
-    os.chdir(cwd)
 
 def main(source, target):
     cwd = os.getcwd()
@@ -88,22 +65,24 @@ def main(source, target):
 
     game_paths = find_all_game_paths(source_path)
     new_game_dirs = get_name_from_paths(game_paths, "_game")
+    print(new_game_dirs)
+    create_directory(target_path)
 
-    create_dir(target_path)
-
-    for src, dest in zip(game_paths, new_game_dirs):
+    # zip matches elements in the two arrays and combine them into a tuple
+    # [1, 2, 3]
+    # ["a", "b", "c"]
+    # output: (1, "a") (2, "b") (3, "c")
+    for src, dest in  zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
-        compile_game_code(dest_path)
-
+    
     json_path = os.path.join(target_path, "metadata.json")
     make_json_metadata_file(json_path, new_game_dirs)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 3:
-        raise Exception("You must pass a source and target directory - only.")
-
+    if (len(args) != 3):
+        raise Exception("You must pass a source and target directory only")
     source, target = args[1:]
     main(source, target)
